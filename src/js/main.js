29d423d27d5294d4df1a4f8f98d31c6e21ed9b80
hiskiapp <hiskianggi@gmail.com>
2024-04-28 22:09:32 +0700

    Add dev-dist folder to .gitignore Update package.json to include vite-plugin-pwa Remove service worker registration in main.js Add PWA-related meta tags and icons in index.html Add vite.config.js for Vite PWA configuration

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,3 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
 import './cam.js'
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('SW registered: ', registration)
-      })
-      .catch((registrationError) => {
-        console.log('SW registration failed: ', registrationError)
-      })
-  })
-}
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,jpg,svg,json,bin}'],
+        maximumFileSizeToCacheInBytes: 5000000
+      },
+      manifest: {
+        name: 'Caledi',
+        short_name: 'Caledi',
+        description: 'Cassava Leaf Disease Classification',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ]
+})
